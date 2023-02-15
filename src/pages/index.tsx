a7e0aba380129f255579fe40fc808380c48c42dc
mahfantuci <maria@imaginationmedia.com>
2023-02-15 19:44:51 -0300

    Add a propriety to make the project built again and save the data on the cache

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { stripe } from '../lib/stripe';
 
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Image from 'next/image';
 
 import { HomeContainer, Product } from '../styles/pages/home';
@@ -49,7 +49,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -69,6 +69,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return{
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2, //2 hours
   }
 }
