a62f8efb22dbc7e7450eebd44e1af38a05f40a62
mariafantuci <mariafantuci@gmail.com>
2023-02-15 20:22:44 -0300

    create the product page layout and styled

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,26 @@
+import Image from 'next/image'
+
+import { ImageContainer, ProductContainer, ProductDetails } from '@/src/styles/pages/product'
 import { useRouter } from 'next/router'
 
 export default function Product(){
     const { query } = useRouter()
     return (
-        <h1>Product: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+          <ImageContainer>
+            <Image width={520} height={480} src="https://www.nicepng.com/png/detail/206-2067446_minions-transparent-images-minion-wallpaper-for-redmi-note.png" alt="" />
+          </ImageContainer>
+    
+          <ProductDetails>
+            <h1>Camiseta X</h1>
+            <span>R$ 79,90</span>
+    
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores aliquid rerum exercitationem facere a molestiae ut sed velit non mollitia? Officiis hic velit assumenda aspernatur nihil, sint sed laboriosam tempora?</p>
+    
+            <button>
+              Comprar agora
+            </button>
+          </ProductDetails>
+        </ProductContainer>
     )
 }
