ced16bf65e59d0d97c4c69d53b2e8a40dd52c677
mahfantuci <maria@imaginationmedia.com>
2023-02-15 17:35:01 -0300

    create the home struct and styled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,41 @@
-import { styled } from '../styles'
-
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  color: 'white',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  }
-
-})
+import Image from 'next/image';
+import { HomeContainer, Product } from '../styles/pages/home';
+import shirt1 from '../assets/shirt/1.png'
+import shirt2 from '../assets/shirt/2.png'
+import shirt3 from '../assets/shirt/3.png'
+import shirt4 from '../assets/shirt/4.png'
 
 export default function Home() {
   return (
-    <Button>Teste</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$: 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={shirt2} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$: 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={shirt3} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$: 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={shirt4} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$: 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
