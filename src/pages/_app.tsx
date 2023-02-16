8f317a17b20490a5a6c4fc7058b1142de13edb30
mariafantuci <mariafantuci@gmail.com>
2023-02-16 16:00:18 -0300

    make the logo svg clickable

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 /* eslint-disable @next/next/no-img-element */
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
@@ -5,13 +6,19 @@ import logoImg from '../assets/logo.svg'
 import { Container, Header } from '../styles/pages/app'
 
 import Image from 'next/image'
+import Link from 'next/link'
+
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={logoImg} alt="" />
+        <Link href="/">
+          <a>
+            <Image src={logoImg} alt="" />
+          </a>
+        </Link>
       </Header>
 
       <Component {...pageProps} />
